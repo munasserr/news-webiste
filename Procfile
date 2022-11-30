24da119834261cd565044e6cd9f8bd71c5218b51
munasser <mohnasser37@gmail.com>
2022-11-30 06:24:57 +0200

    Deploying To Railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn newsBase.wsgi:application --log-file -
---
+++ newsApi/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import requests , random
+
+API_KEY = "a3343bd6c2834603a012551717f67c2b"
+
+def home(request):
+    url = f'https://newsapi.org/v2/top-headlines&apiKey={API_KEY}'
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+    if country :
+        url= f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+    elif category:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    articles = data['articles']
+
+
+    context = {
+        'art' : articles,
+    } 
+
+    return render(request , 'index.html' , context)
